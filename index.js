961b53f27352b4413aedeaf5d1bb9180aa14f5dd
unknown <Pradeepvk927@gmail.com>
2024-03-15 02:19:11 +0530

    Path Added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
+const path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
